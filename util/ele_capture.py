ff736ff6309f1afba57c46ab51c4aff8efc35b1d
roleecorn <roleecorn@gmail.com>
2023-06-04 15:12:43 +0800

    method2

--- util/ele_capture.py
+++ util/ele_capture.py
@@ -12,22 +12,19 @@ def capture(ele: WebElement, driver: webdriver.Chrome, path: Path):
     """
     輸入一個網頁物件，並對他進行快照，之後將圖片儲存在本地
     """
-    location = element.location
-    size = element.size
+    # location = element.location
+    # size = element.size
+    
+    # driver.save_screenshot("shot.png")
 
-    driver.save_screenshot("shot.png")
-
-    x = location['x']
-    y = location['y']
-    w = size['width']
-    h = size['height']
-    width = x + w
-    height = y + h
+    # x = location['x']
+    # y = location['y']
+    # w = size['width']
+    # h = size['height']
+    # width = x + w
+    # height = y + h
     filename = str(uuid.uuid4()[:8])+'.png'
-
-    im = Image.open('shot.png')
-    im = im.crop((int(x), int(y), int(width), int(height)))
-    im.save(path / filename)
+    ele.screenshot_as_png(path / filename)
     return path / filename
 
 
