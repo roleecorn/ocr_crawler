45675b406546d05d46c420715461bf36313f69c4
Sun-I-Fu <jeff77122000@gmail.com>
2023-06-04 20:43:14 +0800

    ele_capture_done

--- util/ele_capture.py
+++ util/ele_capture.py
@@ -1,10 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.remote.webelement import WebElement
 from pathlib import Path
-from .new_driver import new_driver
-from PIL import Image
+from new_driver import new_driver
 import uuid
 
 
@@ -12,28 +9,21 @@ def capture(ele: WebElement, driver: webdriver.Chrome, path: Path):
     """
     輸入一個網頁物件，並對他進行快照，之後將圖片儲存在本地
     """
-    # location = element.location
-    # size = element.size
-    
-    # driver.save_screenshot("shot.png")
-
-    # x = location['x']
-    # y = location['y']
-    # w = size['width']
-    # h = size['height']
-    # width = x + w
-    # height = y + h
-    filename = str(uuid.uuid4()[:8])+'.png'
-    ele.screenshot_as_png(path / filename)
+    uuid_obj = uuid.uuid4()
+    filename = str(uuid_obj)[:8]+'.png'
+    ele.screenshot(str(path / filename))
     return path / filename
 
 
 if __name__ == "__main__":
-    driver_path = Path('')
+    import time
+    myhome = Path.cwd().parent
+    driver_path = myhome / '.wdm' / 'drivers' / 'chromedriver' / 'win32' / '113.0.5672' / 'chromedriver.exe'
     driver = new_driver(driver_path)
 
     driver.get('https://www.google.co.in')
-
-    element = driver.find_element_by_id("lst-ib")
-    file_path = capture(ele=element, driver=driver, path=Path.cwd())
+    time.sleep(5)
+    element = driver.find_element('class name', 'lnXdpd')
+    file_path = capture(ele=element, driver=driver, path=myhome / 'src')
+    driver.close()
     print(file_path)
