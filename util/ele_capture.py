4274c8824c76e187051495e1e2e741db8c6f3322
Sun-I-Fu <jeff77122000@gmail.com>
2023-06-04 20:49:01 +0800

    _

--- util/ele_capture.py
+++ util/ele_capture.py
@@ -1,4 +1,3 @@
-from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
 from pathlib import Path
 from new_driver import new_driver
@@ -18,7 +17,8 @@ def capture(ele: WebElement, path: Path):
 if __name__ == "__main__":
     import time
     myhome = Path.cwd().parent
-    driver_path = myhome / '.wdm' / 'drivers' / 'chromedriver' / 'win32' / '113.0.5672' / 'chromedriver.exe'
+    driver_path = myhome / '.wdm' / 'drivers' / 'chromedriver' / 'win32'
+    driver_path = driver_path / '113.0.5672' / 'chromedriver.exe'
     driver = new_driver(driver_path)
 
     driver.get('https://www.google.co.in')
